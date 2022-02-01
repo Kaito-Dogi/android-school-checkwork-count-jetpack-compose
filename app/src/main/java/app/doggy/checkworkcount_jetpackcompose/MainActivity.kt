bf3af614244db87a0de9d0b4cdb85d6da9b94538
Kaito-Dogi <kendobu0405@gmail.com>
2022-02-02 03:47:14 +0900

    add: Counterの実装

--- app/src/main/java/app/doggy/checkworkcount_jetpackcompose/MainActivity.kt
+++ app/src/main/java/app/doggy/checkworkcount_jetpackcompose/MainActivity.kt
@@ -2,10 +2,67 @@ package app.doggy.checkworkcount_jetpackcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Counter()
+        }
     }
+}
+
+@Composable
+fun Counter() {
+    val padding = 16.dp
+    var count by remember {
+        mutableStateOf(0)
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(padding)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ) {
+        Text(
+            text = count.toString(),
+            style = MaterialTheme.typography.h2,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxWidth(),
+        )
+        Button(
+            onClick = {
+                count += 1
+            },
+        ) {
+            Text(
+                text = "PLUS",
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCounter() {
+    Counter()
 }
